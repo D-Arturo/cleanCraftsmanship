458078309c177d711e42eb8b8575af5eff672a99
D-Arturo <arturomf88@gmail.com>
2023-03-06 17:13:02 +0100

    feat: Stack can be created and it is initially empty

--- src/tests/StackTest.java
+++ src/tests/StackTest.java
@@ -3,10 +3,13 @@ package tests;
 import org.junit.jupiter.api.Test;
 import stack.Stack;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class StackTest {
 
     @Test
     public void canCreateSTack() throws Exception {
         Stack stack = new Stack();
+        assertTrue(stack.isEmpty());
     }
 }
