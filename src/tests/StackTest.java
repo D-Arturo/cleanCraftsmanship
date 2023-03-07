f33955ca21a664c3af597840aac2eaea7e37e763
D-Arturo <arturomf88@gmail.com>
2023-03-07 16:52:48 +0100

    feat: test for a new element to be pushed

--- src/tests/StackTest.java
+++ src/tests/StackTest.java
@@ -3,6 +3,7 @@ package tests;
 import org.junit.jupiter.api.Test;
 import stack.Stack;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class StackTest {
@@ -11,5 +12,12 @@ public class StackTest {
     public void canCreateSTack() throws Exception {
         Stack stack = new Stack();
         assertTrue(stack.isEmpty());
+    }
+
+    @Test
+    public void canPush() throws Exception {
+        Stack stack = new Stack();
+        stack.push(1);
+        assertFalse(stack.isEmpty());
     }
 }
