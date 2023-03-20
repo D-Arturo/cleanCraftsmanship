ff0da09f0ad753d4992d0a672beca948d1590f22
D-Arturo <arturomf88@gmail.com>
2023-03-20 23:41:14 +0100

    refactor: legibility and redundant parameters

--- src/stack/Stack.java
+++ src/stack/Stack.java
@@ -2,13 +2,12 @@ package stack;
 
 public class Stack {
 
-    private boolean isEmpty = true;
+    private final boolean isEmpty = true;
     private int actualSize = 0;
     private final int[] currentValue = new int[2];
 
 
     public void push(int newElement) {
-        setEmpty(false);
         if (size() + 1 >= 3) throw new Overflow();
         currentValue[size()] = newElement;
         increaseStackSizeByOne();
@@ -16,7 +15,6 @@ public class Stack {
 
     public int pop() {
         if (isEmpty()) throw new Underflow();
-        if (isEmpty()) setEmpty(true);
         decreaseSizeByOne();
         return currentValue[size()];
     }
@@ -30,11 +28,7 @@ public class Stack {
     }
 
     public boolean isEmpty() {
-        return isEmpty = size() == 0;
-    }
-
-    public void setEmpty(boolean empty) {
-        isEmpty = empty;
+        return size() == 0;
     }
 
     public int size() {
