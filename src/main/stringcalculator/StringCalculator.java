56deb85a425621bcf1b32aed53095c69e57f2082
D-Arturo <arturomf88@gmail.com>
2023-03-22 18:36:29 +0100

    refactor: equals string method usage

--- src/main/stringcalculator/StringCalculator.java
+++ src/main/stringcalculator/StringCalculator.java
@@ -2,7 +2,8 @@ package main.stringcalculator;
 
 public class StringCalculator {
     public int add(String stringReceived) {
-        if (stringReceived == "4") return 4;
+
+        if (stringReceived.equals("4")) return 4;
         return 0;
     }
 }
