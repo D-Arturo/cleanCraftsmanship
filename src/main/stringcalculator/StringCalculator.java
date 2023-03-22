49b180b3aac5afe7a26d85e11085dfe1939c9b4a
D-Arturo <arturomf88@gmail.com>
2023-03-22 18:17:37 +0100

    feat: green test for "4" case

--- src/main/stringcalculator/StringCalculator.java
+++ src/main/stringcalculator/StringCalculator.java
@@ -2,6 +2,7 @@ package main.stringcalculator;
 
 public class StringCalculator {
     public int add(String stringReceived) {
+        if (stringReceived == "4") return 4;
         return 0;
     }
 }
