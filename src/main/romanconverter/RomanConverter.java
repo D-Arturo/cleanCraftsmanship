bc47ee6d9b24344818000c773ca5df56def0f38d
D-Arturo <arturomf88@gmail.com>
2023-03-23 23:08:23 +0100

    feat: green test for number III

--- src/main/romanconverter/RomanConverter.java
+++ src/main/romanconverter/RomanConverter.java
@@ -4,6 +4,7 @@ public class RomanConverter {
     public static int convertToArabic(String romanNumber) {
         if (romanNumber.equals("I")) return 1;
         if (romanNumber.equals("II")) return 2;
-        return 3;
+        if (romanNumber.equals("III")) return 3;
+        return 4;
     }
 }
