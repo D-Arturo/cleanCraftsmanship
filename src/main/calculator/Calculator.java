057454f7d6f5599e00bd01ccf0d65faac7459483
D-Arturo <arturomf88@gmail.com>
2023-03-25 13:18:07 +0100

    feat: prime factors initial cases, test for number 4

---
+++ src/main/calculator/Calculator.java
@@ -0,0 +1,18 @@
+package main.calculator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Calculator {
+    public static List<Integer> primeFactorsOf(int number) {
+        ArrayList<Integer> primeFactors = new ArrayList<>();
+        if (number > 1) {
+            primeFactors.add(number);
+            if (number % 2 == 0 && number / 2 != 1) {
+                primeFactors.add(number / 2);
+            }
+
+        }
+        return primeFactors;
+    }
+}
---
+++ src/tests/calculator/CalculatorTest.java
@@ -0,0 +1,20 @@
+package tests.calculator;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static main.calculator.Calculator.primeFactorsOf;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+
+    @Test
+    public void ShouldReturnPrimeFactors_WhenNumberReceived() throws Exception {
+        assertEquals(new ArrayList<>(), primeFactorsOf(1));
+        assertEquals(new ArrayList<>(List.of(2)), primeFactorsOf(2));
+        assertEquals(new ArrayList<>(List.of(3)), primeFactorsOf(3));
+        assertEquals(new ArrayList<>(List.of(2, 2)), primeFactorsOf(4));
+    }
+}
