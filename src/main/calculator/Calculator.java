d4e46cb976ec421edf404ce102979f137ad38466
D-Arturo <arturomf88@gmail.com>
2023-03-28 21:43:00 +0200

    Calculator:  refactor

--- src/main/calculator/Calculator.java
+++ src/main/calculator/Calculator.java
@@ -6,16 +6,13 @@ import java.util.List;
 public class Calculator {
     public static List<Integer> primeFactorsOf(int number) {
         ArrayList<Integer> primeFactors = new ArrayList<>();
-        if (number > 1) {
-            for (; number % 2 == 0 && number / 2 != 1; number /= 2) {
-                primeFactors.add(2);
+
+        for (int divisor = 2; number > 1; divisor++) {
+            for (; number % divisor == 0; number /= divisor) {
+                primeFactors.add(divisor);
             }
-            for (; number % 3 == 0 && number / 3 != 1; number /= 3) {
-                primeFactors.add(3);
-            }
-            if (number > 1)
-                primeFactors.add(number);
         }
+
         return primeFactors;
     }
 }
