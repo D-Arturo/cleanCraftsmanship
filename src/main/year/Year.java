42c1d9f4242375a2f991ab9bdac5dede4bf6a12f
D-Arturo <arturomf88@gmail.com>
2023-03-30 19:04:38 +0200

    Leap Year: last test passing

--- src/main/year/Year.java
+++ src/main/year/Year.java
@@ -2,6 +2,7 @@ package main.year;
 
 public class Year {
     public static boolean isLeapYear(int year) {
+        if (year % 4 == 0 && year % 100 == 0 && year % 400 != 0) return false;
         return year % 4 == 0;
     }
 }
