94d456daf0426841cbc77b7c0836ced0952d25f4
D-Arturo <arturomf88@gmail.com>
2023-03-30 19:04:22 +0200

    Leap Year: new test failing

--- src/tests/year/YearTest.java
+++ src/tests/year/YearTest.java
@@ -3,26 +3,29 @@ package tests.year;
 import main.year.Year;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class YearTest {
 
     @Test
     public void shouldReturnFalse_WhenYearNotDivisiblebyFour() throws Exception {
-        Year year = new Year();
-        assertEquals(false, Year.isLeapYear(1997));
+        assertFalse(Year.isLeapYear(1997));
     }
 
     @Test
     public void shouldReturnTrue_WhenYearDivisiblebyFour() throws Exception {
-        Year year = new Year();
-        assertEquals(true, Year.isLeapYear(1996));
+        assertTrue(Year.isLeapYear(1996));
     }
 
     @Test
     public void shouldReturnTrue_WhenYearDivisiblebyFourHundred() throws Exception {
-        Year year = new Year();
-        assertEquals(true, Year.isLeapYear(1600));
+        assertTrue(Year.isLeapYear(1600));
+    }
+
+    @Test
+    public void shouldReturnFalse_WhenYearNotDivisiblebyFourHundred() throws Exception {
+        assertFalse(Year.isLeapYear(1800));
     }
 
 }
