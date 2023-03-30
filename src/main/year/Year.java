0696acdb41fdfae11894c722c3e34dff50eb584a
D-Arturo <arturomf88@gmail.com>
2023-03-30 19:53:15 +0200

    Leap Year: refactor

--- src/main/year/Year.java
+++ src/main/year/Year.java
@@ -2,10 +2,8 @@ package main.year;
 
 public class Year {
     public static boolean isLeapYear(int year) {
-        if (isDivisibleBy(year, 4)) {
-            return !isDivisibleBy(year, 100) || isDivisibleBy(year, 400);
-        }
-        return false;
+        if (!isDivisibleBy(year, 4)) return false;
+        return !isDivisibleBy(year, 100) || isDivisibleBy(year, 400);
     }
 
     private static boolean isDivisibleBy(int year, int factor) {
