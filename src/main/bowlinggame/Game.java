5a4a8df5c894166b4329671292cde23914f08d58
D-Arturo <arturomf88@gmail.com>
2023-03-31 16:20:02 +0200

    BowlingGame: green test for detecting a proper frame

--- src/main/bowlinggame/Game.java
+++ src/main/bowlinggame/Game.java
@@ -3,12 +3,23 @@ package main.bowlinggame;
 public class Game {
 
     private int total = 0;
+    private final int[] rolls = new int[21];
+    private int currentRoll = 0;
 
     public void roll(int pins) {
-        total += pins;
+        rolls[currentRoll] = pins;
+        currentRoll++;
     }
 
     public int score() {
+        for (int i = 0; i < rolls.length; i++) {
+            total += rolls[i];
+            if (i > 1 && i % 2 == 0) {
+                if (rolls[i - 2] + rolls[i - 1] == 10) {
+                    total += rolls[i];
+                }
+            }
+        }
         return total;
     }
 }
