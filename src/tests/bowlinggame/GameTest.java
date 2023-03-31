894b38b8e2c92ef2417b311d1cc8782d70fc21ff
D-Arturo <arturomf88@gmail.com>
2023-03-31 13:56:25 +0200

    BowlingGame: initial test fixed

--- src/tests/bowlinggame/GameTest.java
+++ src/tests/bowlinggame/GameTest.java
@@ -1,6 +1,9 @@
 package tests.bowlinggame;
 
+import main.bowlinggame.Game;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GameTest {
 
@@ -10,6 +13,6 @@ public class GameTest {
         for (int i = 1; i <= 20; i++) {
             game.roll(0);
         }
-        assertEquals(0, game.score(0));
+        assertEquals(0, game.score());
     }
 }
