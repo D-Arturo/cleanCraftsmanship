d311b55631e640974d4eb2def713091678a57ddb
D-Arturo <arturomf88@gmail.com>
2023-03-31 14:59:29 +0200

    BowlingGame: refactor

--- src/tests/bowlinggame/GameTest.java
+++ src/tests/bowlinggame/GameTest.java
@@ -1,27 +1,41 @@
 package tests.bowlinggame;
 
 import main.bowlinggame.Game;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GameTest {
 
+    public Game game = new Game();
+
+    @BeforeEach
+    public void initEach() {
+        Game game = new Game();
+    }
+
+    private void rollSame(int pins, int n) {
+        for (int i = 1; i <= n; i++) {
+            game.roll(pins);
+        }
+    }
+
     @Test
     public void ShouldScoreZero_WhenZeroPinsKnocked() {
-        Game game = new Game();
-        for (int i = 1; i <= 20; i++) {
-            game.roll(0);
-        }
+        rollSame(0, 20);
         assertEquals(0, game.score());
     }
 
     @Test
     public void ShouldScoreTwenty_WhenAllOnesPinsKnocked() {
-        Game game = new Game();
-        for (int i = 1; i <= 20; i++) {
-            game.roll(1);
-        }
+        rollSame(1, 20);
         assertEquals(20, game.score());
+    }
+
+    @Test
+    public void ShouldScoreEighty_WhenAllFoursPinsKnocked() {
+        rollSame(4, 20);
+        assertEquals(80, game.score());
     }
 }
