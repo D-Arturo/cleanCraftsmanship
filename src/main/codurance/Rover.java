1319c00055088fe5804dba047b82adecc3cb1f25
D-Arturo <arturomf88@gmail.com>
2023-04-02 00:34:56 +0200

    Rover: refactor to move Direction enum

--- src/main/codurance/Rover.java
+++ src/main/codurance/Rover.java
@@ -13,47 +13,8 @@ public class Rover {
                 direction = direction.left();
             }
         }
-        return "0:0:" + direction.value;
+        return "0:0:" + direction.value();
     }
 
 
-    enum Direction {
-        NORTH("N", "W", "E"),
-        SOUTH("S", "E", "W"),
-        EAST("E", "N", "S"),
-        WEST("W", "S", "N");
-
-        private final String value;
-        private final String left;
-        private final String right;
-
-        Direction(String value, String left, String right) {
-
-            this.value = value;
-            this.left = left;
-            this.right = right;
-        }
-
-        public Direction right() {
-            return nextDirection(right);
-        }
-
-        private Direction nextDirection(String right) {
-            for (Direction potentialDirection : values()) {
-                if (right == potentialDirection.value) {
-                    return potentialDirection;
-                }
-            }
-            return null;
-        }
-
-        public Direction left() {
-            for (Direction potentialDirection : values()) {
-                if (this.left == potentialDirection.value) {
-                    return potentialDirection;
-                }
-            }
-            return null;
-        }
-    }
 }
