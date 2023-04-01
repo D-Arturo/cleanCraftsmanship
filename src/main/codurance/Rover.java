7f50a07cc76c4ff85c9eb5489b2521f51598f77b
D-Arturo <arturomf88@gmail.com>
2023-04-01 18:47:24 +0200

    Rover: refactor after rotate_right_four_times green test

--- src/main/codurance/Rover.java
+++ src/main/codurance/Rover.java
@@ -6,16 +6,20 @@ public class Rover {
 
     public String execute(String commands) {
         for (char c : commands.toCharArray()) {
-            if (direction == "W") {
-                direction = "N";
-            } else if (direction == "S") {
-                direction = "W";
-            } else if (direction == "E") {
-                direction = "S";
-            } else {
-                direction = "E";
-            }
+            direction = rotateRight();
         }
         return "0:0:" + direction;
+    }
+
+    private String rotateRight() {
+        if (direction == "N") {
+            return "E";
+        } else if (direction == "E") {
+            return "S";
+        } else if (direction == "S") {
+            return "W";
+        } else {
+            return "N";
+        }
     }
 }
