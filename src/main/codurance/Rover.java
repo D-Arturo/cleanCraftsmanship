cd9286d6e616c037403b651042e3567d2cb8fd05
D-Arturo <arturomf88@gmail.com>
2023-04-02 00:12:11 +0200

    Rover: refactor with enum and right method

--- src/main/codurance/Rover.java
+++ src/main/codurance/Rover.java
@@ -3,10 +3,15 @@ package main.codurance;
 public class Rover {
 
     private String direction = "N";
+    private Direction eDirection = Direction.NORTH;
 
     public String execute(String commands) {
         for (char c : commands.toCharArray()) {
-            if (c == 'R') direction = rotateRight();
+            if (c == 'R') {
+                eDirection = eDirection.right();
+                direction = eDirection.value;
+
+            }
             if (c == 'L') direction = rotateLeft();
         }
         return "0:0:" + direction;
@@ -24,5 +29,32 @@ public class Rover {
         if (direction == "E") return "S";
         if (direction == "S") return "W";
         return "N";
+    }
+
+    enum Direction {
+        NORTH("N", "W", "E"),
+        SOUTH("S", "E", "W"),
+        EAST("E", "N", "S"),
+        WEST("W", "S", "N");
+
+        private final String value;
+        private final String left;
+        private final String right;
+
+        Direction(String value, String left, String right) {
+
+            this.value = value;
+            this.left = left;
+            this.right = right;
+        }
+
+        public Direction right() {
+            for (Direction potentialDirection : values()) {
+                if (this.right == potentialDirection.value) {
+                    return potentialDirection;
+                }
+            }
+            return null;
+        }
     }
 }
